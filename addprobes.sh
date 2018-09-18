f1edba634142f47552608794285fd759a19bd711
Paul A. Clarke <pc@us.ibm.com>
2018-09-18 12:19:41 -0500

    add "--delete" to addprobes.sh
    Probes can now be deleted by:
    ```
    # ./addprobes.sh --delete
    ```
    
    Note that all probes are deleted with a single call to `perf probe`.
    
    Also, modifed the path for adding probes to only call `perf probe` once.
    
    Signed-off-by: Paul A. Clarke <pc@us.ibm.com>

--- addprobes.sh
+++ addprobes.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
-# entry, init, release
-for sdt in init entry acquired release; do
-	perf probe --add "sdt_libpthread:mutex_$sdt"
-	for probe in $(perf list | grep 'sdt_libpthread:mutex_'$sdt'.*Tracepoint event' | awk '{print $1}'); do
-		echo $probe
+if [ "$1" = "--delete" ]; then
+	pattern="$(sep=''; echo -n "'"; for sdt in init entry acquired release; do echo -n $sep'sdt_libpthread:mutex_'$sdt'.*Tracepoint event'; sep='|'; done; echo "'")"
+	sdts="$(perf list | eval egrep "$pattern" | awk '{print $1}')"
+	if [ -z "$sdts" ]; then
+		echo "No probes found."
+		exit 1
+	fi
+	perfarg="probe"
+	for sdt in $sdts; do
+		perfarg="$perfarg --del $sdt"
 	done
+	set -x
+	exec perf $perfarg
+fi
+
+perfarg="probe"
+for sdt_mutex in init entry acquired release; do
+	perfarg="$perfarg --add sdt_libpthread:mutex_$sdt_mutex"
 done
+echo "$perfarg"
+perf $perfarg
