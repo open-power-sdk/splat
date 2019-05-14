bba93bd482e7cf8e3ed9f8d25bcc620752425150
Paul A. Clarke <pc@us.ibm.com>
2019-05-13 23:51:28 -0500

    Add support for Advance Toolchain
    `addprobes.sh` now looks for libraries in standard paths for
    Advance Toolchain, adds those libraries to the `perf` buildid-cache,
    and adds probes for interesting tracepoints.  `--delete` will also
    delete these probes.
    
    `--force` is now passed to `perf probe` to allow duplicate events
    with the same name.
    
    Also, minor fix for when the `perf` command is displayed before being run.
    
    Signed-off-by: Paul A. Clarke <pc@us.ibm.com>

--- addprobes.sh
+++ addprobes.sh
@@ -1,32 +1,45 @@
 #!/bin/sh
 
+LIB=/lib64/libpthread.so.0
+
+AT_LIBS=/opt/at*.*/lib64/libpthread.so.0
+
 perfarg="probe"
 
 if [ "$1" = "--delete" ]; then
-	perf probe -x /lib64/libpthread.so.0 --del=pthread_mutex_trylock --del=pthread_mutex_trylock_ret
+	pattern="$(sep=''; echo -n "'"; for probe in trylock trylock_ret; do echo -n $sep'probe_libpthread:pthread_mutex_'$probe'.*Tracepoint event'; sep='|'; done; echo "'")"
+	probes="$(perf list | eval egrep "$pattern" | awk '{print $1}')"
 
 	pattern="$(sep=''; echo -n "'"; for sdt in init entry acquired release; do echo -n $sep'sdt_libpthread:mutex_'$sdt'.*Tracepoint event'; sep='|'; done; echo "'")"
 	sdts="$(perf list | eval egrep "$pattern" | awk '{print $1}')"
-	if [ -z "$sdts" ]; then
+
+	if [ -z "$probes$sdts" ]; then
 		echo "No probes found."
 		exit 1
 	fi
-	for sdt in $sdts; do
-		perfarg="$perfarg --del $sdt"
+
+	for probe in $probes $sdts; do
+		perfarg="$perfarg --del $probe"
 	done
-	set -x
 	exec perf $perfarg
 fi
 
-perf "$perfarg" -x /lib64/libpthread.so.0 --add='pthread_mutex_trylock mutex' --add=pthread_mutex_trylock_ret='pthread_mutex_trylock%return $retval'
-if [ $? -ne 0 ]; then
-	echo "Failed to probe events with symbols in libpthread.so.  You may need to install glibc-debuginfo package."
-	exit 1
-fi
+for lib in $AT_LIBS; do
+	perf buildid-cache --add $lib
+done
 
+for lib in "$LIB" $AT_LIBS; do
+	perf "$perfarg" -x "$lib" --force --add='pthread_mutex_trylock mutex' --add=pthread_mutex_trylock_ret='pthread_mutex_trylock%return $retval'
+	if [ $? -ne 0 -a "$lib" = "$LIB" ]; then
+		echo "Failed to probe events with symbols in $lib.  You may need to install glibc-debuginfo package."
+		exit 1
+	fi
+done
+
+perfarg="$perfarg --force"
 for sdt_mutex in init entry acquired release; do
 	perfarg="$perfarg --add sdt_libpthread:mutex_$sdt_mutex"
 done
 
-echo "$perfarg"
+echo perf "$perfarg"
 perf $perfarg
